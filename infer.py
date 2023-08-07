81692edc36a7e6ccb5b8192b100a9618b1b38134
ssbuild <462304@qq.cn>
2023-08-07 16:54:28 +0800

    llama quart
    Signed-off-by: ssbuild <462304@qq.cn>

--- infer.py
+++ infer.py
@@ -9,10 +9,15 @@ from transformers import HfArgumentParser
 from data_utils import train_info_args, NN_DataHelper, get_deepspeed_config
 from aigc_zoo.model_zoo.llm.llm_model import MyTransformer
 from aigc_zoo.utils.llm_generate import Generate
-
+from deep_training.utils.hf import register_transformer_model
+from deep_training.nlp.models.rellama.modeling_llama import ReLlamaForCausalLM
+from transformers import AutoModelForCausalLM
 deep_config = get_deepspeed_config()
 
 if __name__ == '__main__':
+    #导入模型
+    if train_info_args['model_type'] == 'llama':
+        register_transformer_model(ReLlamaForCausalLM,AutoModelForCausalLM)
     parser = HfArgumentParser((ModelArguments, DataArguments))
     model_args, data_args = parser.parse_dict(train_info_args, allow_extra_keys=True)
 
@@ -21,8 +26,19 @@ if __name__ == '__main__':
 
     pl_model = MyTransformer(config=config, model_args=model_args,torch_dtype=config.torch_dtype,)
     model = pl_model.get_llm_model()
-
-    model.eval().half().cuda()
+    model = model.eval()
+    if hasattr(model,'quantize'):
+        if not model.quantized:
+            # 按需修改，目前只支持 4/8 bit 量化 ， 可以保存量化模型
+            model.half().quantize(4).cuda()
+            # 保存量化权重
+            # model.save_pretrained('qwen-chat-7b-int4',max_shard_size="4GB")
+            # exit(0)
+        else:
+            # 已经量化
+            model.half().cuda()
+    else:
+        model.half().cuda()
 
     text_list = ["写一个诗歌，关于冬天",
                  "晚上睡不着应该怎么办",
