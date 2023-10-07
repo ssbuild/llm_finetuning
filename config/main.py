d2ab44c2ab388bf5022951c18672ec186938b84d
ssbuild <462304@qq.cn>
2023-10-07 17:28:07 +0800

    0.2.5
    Signed-off-by: ssbuild <462304@qq.cn>

--- config/main.py
+++ config/main.py
@@ -10,7 +10,7 @@ from transformers import BitsAndBytesConfig
 global_args = {
     # 训练配置
     **dict(
-        trainer_backend ='pl', # one of pl , hf
+        trainer_backend ='pl', # one of pl , hf , cl
         enable_deepspeed = False,
         enable_ptv2 = False,
         enable_lora = True,
@@ -35,16 +35,20 @@ global_args = {
 
 
 
-if global_args["enable_lora"]:
-    from config.sft_config_lora import train_info_args,train_info_args_hf,train_model_config
-elif global_args["enable_ptv2"]:
-    from config.sft_config_ptv2 import train_info_args,train_info_args_hf,train_model_config
-else:
-    from config.sft_config import train_info_args,train_info_args_hf,train_model_config
 
+if global_args["enable_lora"]:
+    from config.sft_config_lora import train_info_args,train_info_args_hf,train_info_args_colossalai,train_model_config
+elif global_args["enable_ptv2"]:
+    from config.sft_config_ptv2 import train_info_args,train_info_args_hf,train_info_args_colossalai,train_model_config
+else:
+    from config.sft_config import train_info_args,train_info_args_hf,train_info_args_colossalai,train_model_config
+
+assert global_args["trainer_backend"] in ["pl","hf","cl"]
 
 if global_args["trainer_backend"] == "hf":
     train_info_args = train_info_args_hf
+elif global_args["trainer_backend"] == "cl":
+    train_info_args = train_info_args_colossalai
 
 
 
@@ -60,9 +64,6 @@ def patch_args(train_info_args):
             global_args["quantization_config"] = None
 
     if global_args["enable_lora"]:
-        #检查lora adalora是否开启
-        if 'lora' not in train_info_args and 'adalora' not in train_info_args:
-            raise ValueError('please config lora or adalora')
         assert train_info_args.get('lora',{}).get('with_lora',False) + \
             train_info_args.get('adalora',{}).get('with_lora',False) + \
             train_info_args.get('ia3',{}).get('with_lora',False) == 1 , ValueError('lora adalora ia3 can set one at same time !')
